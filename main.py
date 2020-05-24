d49d8dc970ef9ca8a0589ae33de9d35238a6e8cf
Phuwadol Viroonluecha <31432955+vladipooh@users.noreply.github.com>
2020-05-24 22:32:30 +0200

    So Tried

--- main.py
+++ main.py
@@ -1,43 +1,54 @@
 import os
-import pickle
-import flask
-import requests
-from datetime import timedelta
-from flask import make_response, request, current_app, render_template
-from functools import update_wrapper
-# from flask_cors import CORS
+from flask import Flask, render_template, request, redirect, url_for
 
-app = flask.Flask(__name__)
-# cors = CORS(app)
+app = Flask(__name__)
 
-# loading my model
-# model = pickle.load(open("mbti.pkl", "rb"))
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
 
-# @app.route('/')
 
-# def home():
-   # return render_template('form.html')
+###
+# Routing for your application.
+###
 
-# @app.route('/test', methods=['GET','POST'])
-# def predict():
-        # url = "/predict"
-        # data = {"feature_array": [1.0, 2.0, 1.0, 1.0, 3.0, 1.0, 1.0, 2.0, 1.0, 2.0, 2.0, 2.0, 4.0]}
-        # r = requests.post(url, json=data)
-        # return r.text
+@app.route('/')
+def home():
+    """Render website's home page."""
+    return render_template('home.html')
 
-# defining a route for only post requests
-# @app.route('/predict', methods=['POST'])
-# def index():
-    # getting an array of features from the post request's body
-    # feature_array = request.get_json()['feature_array']
 
-    # creating a response object
-    # storing the model's prediction in the object
-    # response = {}
-    # response['predictions'] = model.predict([feature_array]).tolist()
+@app.route('/about/')
+def about():
+    """Render the website's about page."""
+    return render_template('about.html')
 
-    # returning the response object as json
-    # return flask.jsonify(response)
+
+###
+# The functions below should be applicable to all Flask apps.
+###
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
